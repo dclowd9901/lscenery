b78931faeb7c3e0f4a1efa64190ca9c98e1bab7b
dclowd9901 <david.ddrew@gmail.com>
2013-07-25 17:50:31 -0700

    Fixed json sample

--- sample-model.js
+++ sample-model.js
@@ -1,77 +1,51 @@
-var model = { 
-    wave : {
-        scriptName: '',
-        startWhen: '',
-        startOn: '',
-        whenFinished: '',
-        
-        startPriority: {
-          options: [0, 1],
-          desc: ['Enemies are dead', 'Timer finishes'],
-          value: 0
-        },
-
-        layerName: {
-          options: [],
-          value: null
-        },
-
-        enemies: {
-          leader: {
-            className: {
-              options: [],
-              value: null
+var model = {
+    structure: {
+        a: 50, // input
+        b: { // fieldset>inputs
+            d: 'foo',
+            e: 'bar',
+            moo: {
+                m: 'moo',
+                n: 'noo'
             },
-            weapons: [{
-              className: {
-                options: [],
-                value: null
-              },
-              firingRate: '',
-              firingDirType: {
-                options: [0, 1],
-                desc: [],
-                value: 0
-              },
-              firingAngle: ''
-            }]
-          },
-          followers: [{
-            className: {
-              options: [],
-              value: null
-            },            
-            trailing: '',
-            offset: {
-              x: '',
-              y: ''
-            },
-            weapons: [{
-              className: {
-                options: [],
-                value: null
-              },
-              firingRate: '',
-              firingDirType: {
-                options: [0, 1],
-                desc: ['Static direction', 'At player'],
-                value: 0
-              },
-              firingAngle: ''
-            }]            
-          }]
+            coo: {
+                nofollow: true,
+                coocoo: 'tee',
+                bear: {
+                    bar: 'bean'
+                }
+            }
         },
-        script: {
-          directives: [{
-            starting: {
-              min: 0,
-              max: 1,
-              step: 0.1,
-              value: 0
-            },
-            speed: '',
-            waitTime: ''
-          }]
+        c: [{ // fieldset>ul>li>inputs
+            f: 'baz',
+            g: 'biz'
+        },
+        { // fieldset>ul>li>inputs
+            f: 'bar',
+            g: 'bir'
+        }],
+        h: { // select
+            options: [7, 8, 9, 10],
+            value: 7
+        },        
+        hWithDesc: { // select
+            options: [7, 8, 9, 10],
+            desc: ['Seven', 'Eight', 'Nine', 'Ten'],
+            value: 7
+        },
+        i: { // range
+            min: 0,
+            max: 15,
+            step: 1,
+            value: 12
+        },
+        j: { // radios
+            radios: [7, 8],
+            value: 7
+        },
+        k: { // checkboxes
+            checkboxes: [7, 8],
+            values: [7, 8]
         }
-      }
-  }
+    }
+};
